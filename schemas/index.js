3f3aa76b7f84f76613e75e05543bd4a00ff7fdab
adarshchhetri9 <chhetriadarsh912@gmail.com>
2023-01-30 12:02:49 +0530

    added sanity backend

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,28 @@
-export const schemaTypes = []
+import pin from './pin'
+import postedBy from './postedBy'
+import save from './save'
+import comments from './comments'
+
+export const schemaTypes = [
+  {
+    name: 'user',
+    title: 'User',
+    type: 'document',
+    fields: [
+      {
+        name: 'username',
+        title: 'Username',
+        type: 'string',
+      },
+      {
+        name: 'image',
+        title: 'Image',
+        type: 'string',
+      },
+    ],
+  },
+  pin,
+  postedBy,
+  comments,
+  save,
+]
